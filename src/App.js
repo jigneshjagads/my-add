d8c366aadb06e232e6ecec27d64d47dde71fe463
jigneshjagads <78341372+jigneshjagads@users.noreply.github.com>
2021-05-18 19:09:20 +0530

    init branch

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        //remove p tag
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        //new branch code
       </header>
     </div>
   );
